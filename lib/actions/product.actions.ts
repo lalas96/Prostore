4a147e156052b2bb10b9ab35d71e7d9a1b5b3e43
lalas96 <lalasac1@gmail.com>
2026-05-20 16:55:48 +0200

    Fix Prisma Decimal build issue

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,20 +3,32 @@ import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
-export async function getLatestProducts(){
-    const data = await prisma.product.findMany({
-        take: LATEST_PRODUCTS_LIMIT,
-        orderBy:{createdAt:'desc'},
-    });
-    return convertToPlainObject(data);
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return data.map((p) => ({
+    ...p,
+    price: p.price.toString(),
+    rating: p.rating.toString(),
+  }));
 }
 
-export async function getProductBySlug(slug:string){
-    return await prisma.product.findFirst({
-        where:{slug:slug},
-    })
-}
+export async function getProductBySlug(slug: string) {
+  const product = await prisma.product.findFirst({
+    where: { slug },
+  });
 
+  if (!product) return null;
+
+  return {
+    ...product,
+    price: product.price.toString(),
+    rating: product.rating.toString(),
+  };
+}
 
 
 
